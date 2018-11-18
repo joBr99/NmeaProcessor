6839c5579f8747aa2ad0f96958fa2c3dff17b0d6
Johannes <johannes+develop+github@braun-rheingau.de>
2018-11-18 22:46:27 +0100

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,28 +1,25 @@
--- require 'NmeaProcessor'
+--require 'NmeaProcessor'
 require("gps.NmeaProcessor")
-
 
 gps = NmeaProcessor:new(nil)
 
-rserial=io.open("COM4","r")
+rserial=io.open("/dev/ttyS0","r")
 while true do
-line = nil
+chain = nil
   while chain==nil do
-    line=rserial:read();
-    print(line)
-    
-    
-    gps:processline(line)
-  --print(line)
+    chain=rserial:read();
+    --print(chain)
+    gps:processline(chain)
+  end
+
+  if(math.fmod(os.clock()*1000000,50) == 0) then
     print("Uhrzeit: "..gps:getHour()..":"..gps:getMinute()..":"..gps:getSecond())
     print('GPS Fix: '..tostring(gps:isFixValid()))
     print('Longitude: '..gps:getLongitude())
     print('Latitude: '..gps:getLatitude())
-    print('Altitude: '..gps:getSpeed())
-    print("Datum: "..gps:getDay().."."..gps:getMonth().."."..gps:getYear())
-    
-    
-    
-  end
+    --  print('Altitude: '..gps:getSpeed())
+    print("Datum: "..gps:getDay().."."..gps:getMonth().."."..gps:getYear())  end
 
 end
+
+
